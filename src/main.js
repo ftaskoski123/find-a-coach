7c3dcf44bcce276e61572599397b0e637c2ed805
Filip Tashkoski <filip.taskoski69@gmail.com>
2023-07-14 14:50:06 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+
+// createApp(App).mount('#app')
+
+
+ import { createApp } from 'vue'
+ import router from './router.js';
+ import store from './store/index.js';
+import App from './App.vue'
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
+
