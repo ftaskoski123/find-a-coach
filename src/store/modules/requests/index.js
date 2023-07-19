fd092179f0986f45909accacc071a1db94a310e6
Filip Tashkoski <filip.taskoski69@gmail.com>
2023-07-19 18:39:55 +0200

    Outputting Requests and Filtering Requests

--- src/store/modules/requests/index.js
+++ src/store/modules/requests/index.js
@@ -1,5 +1,6 @@
 import mutations from "./mutations.js";
 import actions from "./actions.js";
+import getters from "./getters.js";
 
 export default{
     namespaced:true,
@@ -10,5 +11,6 @@ export default{
         };
     },
     mutations,
-    actions
+    actions,
+    getters
 };
