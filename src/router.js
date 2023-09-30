684fbf9c006d9a67614556855ef2cbd76d2dc94b
ftaskoski123 <filip.taskoski69@gmail.com>
2023-09-30 15:27:38 +0200

    Updated routing

--- src/router.js
+++ src/router.js
@@ -8,34 +8,46 @@ import UserAuth from "./pages/auth/UserAuth.vue";
 import NotFound from "./pages/NotFound.vue";
 import store from "./store/index.js";
 
-
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: CoachesList },
+    { path: "/coaches", component: CoachesList, meta: { title: "Coaches" } },
     {
       path: "/coaches/:id",
       component: CoachDetail,
       props: true,
       children: [{ path: "contact", component: ContactCoach }],
+      meta: {
+        title: "Contact Coach",
+      },
     },
-    { path: "/register", component: CoachRegistration,meta: { requiresAuth: true } },
-    { path: "/requests", component: RequestsReceived,meta: { requiresAuth: true } },
-    {path:"/auth" ,component:UserAuth,meta: { requiresUnauth: true }},
-    { path:"/:notFound(.*)",component:NotFound },
-
+    {
+      path: "/register",
+      component: CoachRegistration,
+      meta: { requiresAuth: true, title: "Register as a Coach" },
+    },
+    {
+      path: "/requests",
+      component: RequestsReceived,
+      meta: { requiresAuth: true, title: "Requests Received" },
+    },
+    {
+      path: "/auth",
+      component: UserAuth,
+      meta: { requiresUnauth: true, title: "Authentication" },
+    },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
-router.beforeEach(function(to,_,next){
-  if(to.meta.requiresAuth && !store.getters.isAuthenticated){
-    next('/auth');
-  }else if(to.meta.requiresUnauth && store.getters.isAuthenticated){
-    next('/coaches');
-  }else{
+router.beforeEach(function (to, _, next) {
+  document.title = `${to.meta.title} | Find a Coach`;
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next("/auth");
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next("/coaches");
+  } else {
     next();
   }
-
-})
+});
 export default router;
